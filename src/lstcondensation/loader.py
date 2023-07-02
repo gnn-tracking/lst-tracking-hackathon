233cf19868ded6f62d84a3c152d28bc419ecb840
Kilian Lieret <kilian.lieret@posteo.de>
2023-07-01 18:47:46 -0700

    Move model to library

---
+++ src/lstcondensation/loader.py
@@ -0,0 +1,20 @@
+from gnn_tracking.utils.loading import TrackingDataModule
+
+default_data_module = TrackingDataModule(
+    train=dict(
+        dirs=[
+            "/scratch/gpfs/IOJALVO/gnn-tracking/object_condensation/lst_data_v0/processed/"
+        ],
+        stop=150,
+        max_sample_size=800,
+    ),
+    val=dict(
+        dirs=[
+            "/scratch/gpfs/IOJALVO/gnn-tracking/object_condensation/lst_data_v0/processed/"
+        ],
+        start=150,
+        stop=155,
+    ),
+    cpus=3,
+    # could also configure a 'test' set here
+)
---
+++ src/lstcondensation/model.py
@@ -0,0 +1,45 @@
+from gnn_tracking.models.resin import ResIN
+from gnn_tracking.models.track_condensation_networks import ModularGraphTCN
+from pytorch_lightning.core.mixins import HyperparametersMixin
+from torch import Tensor, nn
+from torch_geometric.data import Data
+
+
+class LSGraphTCN(nn.Module, HyperparametersMixin):
+    def __init__(
+        self,
+        *,
+        node_indim: int,
+        edge_indim: int,
+        h_dim=5,
+        e_dim=4,
+        h_outdim=2,
+        hidden_dim=40,
+        L_hc=3,
+        alpha_hc: float = 0.5,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        hc_in = ResIN(
+            node_dim=h_dim,
+            edge_dim=e_dim,
+            object_hidden_dim=hidden_dim,
+            relational_hidden_dim=hidden_dim,
+            alpha=alpha_hc,
+            n_layers=L_hc,
+        )
+        self._gtcn = ModularGraphTCN(
+            hc_in=hc_in,
+            node_indim=node_indim,
+            edge_indim=edge_indim,
+            h_dim=h_dim,
+            e_dim=e_dim,
+            h_outdim=h_outdim,
+            hidden_dim=hidden_dim,
+        )
+
+    def forward(
+        self,
+        data: Data,
+    ) -> dict[str, Tensor]:
+        return self._gtcn.forward(data=data)
