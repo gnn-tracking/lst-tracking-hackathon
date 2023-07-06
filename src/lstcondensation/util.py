b29081fb4eb0c3e2b8b17b56ee120ccc63fe3315
Kilian Lieret <kilian.lieret@posteo.de>
2023-07-05 17:51:02 -0700

    Move configs

--- src/lstcondensation/util.py
+++ src/lstcondensation/util.py
@@ -1,9 +1,9 @@
 import coolname
-from rich import Console
+from rich.console import Console
 
 
 def random_trial_name() -> str:
     c = Console(width=80)
     name = coolname.generate_slug(3)
-    c.rule(name)
+    c.rule(f"[bold][yellow]{name}[/yellow][/bold]")
     return name
