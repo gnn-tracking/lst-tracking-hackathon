957b8e9401be92d80e1edca4db6b9af3830c06ab
Kilian Lieret <kilian.lieret@posteo.de>
2023-07-06 17:57:27 -0700

    WIP on many fronts

--- src/lstcondensation/model.py
+++ src/lstcondensation/model.py
@@ -1,3 +1,4 @@
+from gnn_tracking.models.edge_classifier import ECFromChkpt
 from gnn_tracking.models.resin import ResIN
 from gnn_tracking.models.track_condensation_networks import ModularGraphTCN
 from pytorch_lightning.core.mixins import HyperparametersMixin
@@ -17,6 +18,8 @@ class LSGraphTCN(nn.Module, HyperparametersMixin):
         hidden_dim=40,
         L_hc=3,
         alpha_hc: float = 0.5,
+        ec: ECFromChkpt | None = None,
+        ec_thld: float = 0.5,
     ):
         super().__init__()
         self.save_hyperparameters()
@@ -36,6 +39,10 @@ class LSGraphTCN(nn.Module, HyperparametersMixin):
             e_dim=e_dim,
             h_outdim=h_outdim,
             hidden_dim=hidden_dim,
+            ec=ec,
+            ec_threshold=ec_thld,
+            feed_edge_weights=True,
+            mask_orphan_nodes=True,
         )
 
     def forward(
--- src/lstcondensation/util.py
+++ src/lstcondensation/util.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import coolname
 from rich.console import Console
 
@@ -7,3 +9,17 @@ def random_trial_name() -> str:
     name = coolname.generate_slug(3)
     c.rule(f"[bold][yellow]{name}[/yellow][/bold]")
     return name
+
+
+def find_latest_checkpoint(log_dir: Path):
+    assert log_dir.is_dir()
+    if not log_dir.name == "checkpoints":
+        log_dir /= "checkpoints"
+    print(log_dir)
+    assert log_dir.is_dir()
+    checkpoints = list(log_dir.glob("*.ckpt"))
+    if len(checkpoints) == 0:
+        raise ValueError(f"No checkpoints found in {log_dir}")
+    path = max(checkpoints, key=lambda p: p.stat().st_mtime)
+    print(path)
+    return path
