37582b385624a615adf04e887c6a8836659abd2b
Kilian Lieret <kilian.lieret@posteo.de>
2023-11-16 11:35:00 -0500

    Studying LST connectivity

--- src/lstcondensation/loader.py
+++ src/lstcondensation/loader.py
@@ -3,25 +3,26 @@ from gnn_tracking.utils.loading import TrackingDataModule
 default_data_module = TrackingDataModule(
     train=dict(
         dirs=[
-            "/scratch/gpfs/IOJALVO/gnn-tracking/object_condensation/lst_data_v0/processed/"
+            "/scratch/gpfs/IOJALVO/gnn-tracking/object_condensation/lst_data_v1/processed/"
         ],
         stop=150,
         # sample_size=800,
     ),
     val=dict(
         dirs=[
-            "/scratch/gpfs/IOJALVO/gnn-tracking/object_condensation/lst_data_v0/processed/"
+            "/scratch/gpfs/IOJALVO/gnn-tracking/object_condensation/lst_data_v1/processed/"
         ],
         start=150,
         stop=155,
     ),
     test=dict(
         dirs=[
-            "/scratch/gpfs/IOJALVO/gnn-tracking/object_condensation/lst_data_v0/processed/"
+            "/scratch/gpfs/IOJALVO/gnn-tracking/object_condensation/lst_data_v1/processed/"
         ],
         start=170,
         stop=175,
     ),
     cpus=3,
+    identifier="lst_v1",
     # could also configure a 'test' set here
 )
